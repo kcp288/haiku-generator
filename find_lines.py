36ae84f749089795bf51c0d129615557270c9441
Sergey Bokhnyak <home@172-16-193-3.DYNAPOOL.NYU.EDU>
2015-12-06 18:25:09 -0500

    Now it's fixed

--- find_lines.py
+++ find_lines.py
@@ -9,8 +9,9 @@ def get_lines(t,sentences,important):
 	sec_line = []
 	third_line = []
 	i =0
-	while i < len(five) & len(first_line) == 0:
+	while i < len(five) :
 		for k in range(0,len(five[i])):
+			
 			if len(five[i]) > 0:
 				tup = five[i][k]
 				sent = sentences[i]
@@ -23,9 +24,12 @@ def get_lines(t,sentences,important):
 					break
 				else:
 					first_line = []
+		if len(first_line) > 0:
+			break
 		i +=1
+
 	i =0
-	while i < len(sev) & len(sec_line) == 0:
+	while i < len(sev) :
 		for k in range(0,len(sev[i])):
 			if len(sev[i]) > 0:
 				tup = sev[i][k]
@@ -39,10 +43,14 @@ def get_lines(t,sentences,important):
 					break
 				else:
 					sec_line = []
+		if len(sec_line) > 0:
+			break
 		i +=1
+
 	i=0
-	while i < len(five) & len(third_line) == 0:
+	while i < len(five) :
 		for k in range(0,len(five[i])):
+			print('something')
 			if len(five[i]) > 0:
 				tup = five[i][k]
 				sent = sentences[i]
@@ -55,6 +63,8 @@ def get_lines(t,sentences,important):
 					break
 				else:
 					third_line = []
+		if len(third_line) > 0:
+			break
 		i +=1
 	return [first_line,sec_line,third_line]
 
@@ -64,7 +74,18 @@ def check_important(l,important):
 			return True
 	return False
 
+"""
+sentences = [['we','are','your','friends','dont','ever','go','against'],['you','are','my','lightning','in','a','bottle'],['last','sentence','very','short']]
+f = [[(0,4),(1,5),(2,6)],[(0,3),(1,4),(3,6)],[(0,2),(1,3)]]
+s = [[(0,6),(1,7)],[(0,5),(1,6)],[]]
+tup = (f,s)
+imp = ['friends','lightning','sentence']
 
+out = get_lines(tup,sentences,imp)
+print(out[0])
+print(out[1])
+print(out[2])
+"""
 
 
 #Idea for a better evaluation of which sentences are better pending
